84d962512018b58a049b3ca01c66f033879ab53f
Ethan Wolf <ethan.wolf@twcable.com>
2015-03-18 13:53:45 -0600

    Extract and download what we need to create a deployment (rpm or docker image) of the druid jars

---
+++ package-deployment/create-deployment.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# There are two directories we care about:
+# 1.  The directory where this shell script is deployed
+# 2.  The directory the user is running from where we want to package things to
+# Dynamically determine them.
+if [ -z "$PROG_HOME" ] ; then
+  ## resolve links - $0 may be a link to PROG_HOME
+  PRG="$0"
+
+  # need this for relative symlinks
+  while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+      PRG="$link"
+    else
+      PRG="`dirname "$PRG"`/$link"
+    fi
+  done
+
+  saveddir=`pwd`
+
+  PROG_HOME=`dirname "$PRG"`/..
+
+  # make it fully qualified
+  PROG_HOME=`cd "$PROG_HOME" && pwd`
+  directory=`dirname "$PRG"`
+  cd "$saveddir"
+fi
+
+
+
+PROG_DIR=${directory}
+BASE_DIR=${PROG_DIR}/..
+DRUID_LIB=./lib
+DRUID_DEPS=./deps
+CP=${DRUID_LIB}/*
+
+DRUID_VERSION="0.7.0"
+
+echo BASE_DIR is ${BASE_DIR}
+
+# STEP 1:  Assume druid has been built.  Extract the deployment jars from the
+# tar file that they are packaged in and move them to the DRUID_LIB dir.
+mkdir ${PROG_DIR}/tmp_extraction
+tar xzf ${BASE_DIR}/services/target/druid-${DRUID_VERSION}-bin.tar.gz -C ${PROG_DIR}/tmp_extraction
+mv ${PROG_DIR}/tmp_extraction/druid-${DRUID_VERSION}/lib/ ${DRUID_LIB}
+rm -rf ${PROG_DIR}/tmp_extraction
+
+# STEP 2: Install a local repo of extensions in the DRUID_DEPS dir.  
+java -server -Xmx8g -Xms8g -Duser.timezone=UTC -Dfile.encoding=UTF-8 -Ddruid.extensions.localRepository="${DRUID_DEPS}" -Ddruid.extensions.coordinates="[\"io.druid.extensions:druid-kafka-eight:$DRUID_VERSION\", \"io.druid.extensions:druid-histogram:$DRUID_VERSION\", \"io.druid.extensions:mysql-metadata-storage:$DRUID_VERSION\"]" -cp "$CP" io.druid.cli.Main tools pull-deps
