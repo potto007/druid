b6996934d42daba12a15a4487006bd97f79252d9
Ethan Wolf <ethan.wolf@twcable.com>
2015-03-25 23:26:08 -0600

    Fix a typo.

--- package-deployment/standard-deploy/scripts/start-historical.sh
+++ package-deployment/standard-deploy/scripts/start-historical.sh
@@ -7,7 +7,7 @@ CP=/opt/druid/config/historical:$CP
 : echo ${PROCESSING_BUFFER_SIZE_BYTES:=1073741824}
 : echo ${PROCESSING_NUM_THREADS:=16}
 : echo ${SERVER_PRIORITY:=0}
-: echo ${USE_CACHE:-true}
+: echo ${USE_CACHE:=true}
 
 #  Needs to be based on the size of the local cache disk available to this historical node.
 #  Should really have no default.
@@ -20,7 +20,7 @@ NUM_THREADS_PLUS_ONE=`expr $PROCESSING_NUM_THREADS \\+ 1`
 : ${JVM_MAX_DIRECT_MEM_SIZE:=`expr $PROCESSING_BUFFER_SIZE_BYTES \\* $NUM_THREADS_PLUS_ONE`}
 
 HISTORICAL_JAVA_OPTS="-XX:NewSize=6g -XX:MaxNewSize=6g -XX:+UseConcMarkSweepGC -XX:MaxDirectMemorySize=${JVM_MAX_DIRECT_MEM_SIZE}"
-HISTORICAL_JAVA_PROPS="-Ddruid.server.tier=${HISTORICAL_TIER} -Ddruid.storageDirectory=${DEEPSTORAGE_DIRECTORY} -Ddruid.server.http.numThreads=${HTTP_NUM_THREADS} -Ddruid.processing.buffer.sizeBytes=${PROCESSING_BUFFER_SIZE_BYTES} -Ddruid.processing.numThreads=${PROCESSING_NUM_THREADS} -Ddruid.server.maxSize=${SERVER_MAX_SIZE} -Ddruid.segmentCache.locations=[{\"path\": \"/druid/local/historical/indexCache\", \"maxSize\": ${SERVER_MAX_SIZE}}] -Ddruid.server.priority=${SERVER_PRIORITY} -Ddruid.historical.cache.useCache=${USE_CACHE} -Ddruid.historical.cache.populateCache=${USE_CACHE}"
+HISTORICAL_JAVA_PROPS="-Ddruid.server.tier=${HISTORICAL_TIER} -Ddruid.storageDirectory=${DEEPSTORAGE_DIRECTORY} -Ddruid.server.http.numThreads=${HTTP_NUM_THREADS} -Ddruid.processing.buffer.sizeBytes=${PROCESSING_BUFFER_SIZE_BYTES} -Ddruid.processing.numThreads=${PROCESSING_NUM_THREADS} -Ddruid.server.maxSize=${SERVER_MAX_SIZE} -Ddruid.segmentCache.locations=[{\"path\"\: \"/druid/local/historical/indexCache\", \"maxSize\"\: ${SERVER_MAX_SIZE}}] -Ddruid.server.priority=${SERVER_PRIORITY} -Ddruid.historical.cache.useCache=${USE_CACHE} -Ddruid.historical.cache.populateCache=${USE_CACHE}"
 
 
 java $COMMON_JAVA_PROPS $HISTORICAL_JAVA_OPTS -cp $CP $COMMON_JAVA_OPTS $HISTORICAL_JAVA_PROPS io.druid.cli.Main server historical
