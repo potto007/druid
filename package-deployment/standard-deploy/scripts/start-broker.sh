264d5456c371b7751abf90f326f144fd6ffb4639
Ethan Wolf <ethan.wolf@twcable.com>
2015-04-03 15:29:29 -0600

    Better configuration of caching through env variables.  Sigh

--- package-deployment/standard-deploy/scripts/start-broker.sh
+++ package-deployment/standard-deploy/scripts/start-broker.sh
@@ -2,10 +2,15 @@
 : echo ${JVM_MAX_DIRECT_MEM_SIZE:=64g}
 : echo ${SELECT_TIER:=highestPriority}
 : echo ${MAX_IDLE_TIME:=PT5m}
+: echo ${USE_CACHE:=false}
+: echo ${CACHE_TYPE:=local}
+: echo ${CACHE_PREFIX:=$DRUID_CLUSTER_NAME}
+# 30 days
+: echo ${CACHE_EXPIRATION:=2592000}
 
 CP=/opt/druid/config/broker:$CP
 BROKER_JAVA_OPTS="-XX:NewSize=6g -XX:MaxNewSize=6g -XX:+UseConcMarkSweepGC -XX:MaxDirectMemorySize=${JVM_MAX_DIRECT_MEM_SIZE}"
-BROKER_JAVA_PROPS="-Ddruid.broker.http.numConnections=${NUM_CONNECTIONS_PER_BROKER} -Ddruid.cache.hosts=${MEMCACHED_HOSTS} -Ddruid.broker.select.tier=${SELECT_TIER} -Ddruid.server.http.maxIdleTime=${MAX_IDLE_TIME}"
+BROKER_JAVA_PROPS="-Ddruid.broker.http.numConnections=${NUM_CONNECTIONS_PER_BROKER} -Ddruid.broker.cache.useCache=${USE_CACHE} -Ddruid.broker.cache.populateCache=${USE_CACHE} -Ddruid.cache.type=${CACHE_TYPE} -Ddruid.cache.memcachedPrefix=${CACHE_PREFIX} -Ddruid.cache.hosts=${MEMCACHED_HOSTS} -Ddruid.cache.expiration=${CACHE_EXPIRATION}  -Ddruid.broker.select.tier=${SELECT_TIER} -Ddruid.server.http.maxIdleTime=${MAX_IDLE_TIME}"
 
 JAVA_COMMAND="java $COMMON_JAVA_PROPS $BROKER_JAVA_OPTS -cp $CP $COMMON_JAVA_OPTS $BROKER_JAVA_PROPS io.druid.cli.Main server broker"
 echo $JAVA_COMMAND
--- package-deployment/standard-deploy/scripts/start-historical.sh
+++ package-deployment/standard-deploy/scripts/start-historical.sh
@@ -7,7 +7,11 @@ CP=/opt/druid/config/historical:$CP
 : echo ${PROCESSING_BUFFER_SIZE_BYTES:=1073741824}
 : echo ${PROCESSING_NUM_THREADS:=16}
 : echo ${SERVER_PRIORITY:=0}
-: echo ${USE_CACHE:=true}
+: echo ${USE_CACHE:=false}
+: echo ${CACHE_TYPE:=local}
+: echo ${CACHE_PREFIX:=$DRUID_CLUSTER_NAME}
+# 30 days
+: echo ${CACHE_EXPIRATION:=2592000}
 
 #  Needs to be based on the size of the local cache disk available to this historical node.
 #  Should really have no default.
@@ -20,7 +24,7 @@ NUM_THREADS_PLUS_ONE=`expr $PROCESSING_NUM_THREADS \\+ 1`
 : ${JVM_MAX_DIRECT_MEM_SIZE:=`expr $PROCESSING_BUFFER_SIZE_BYTES \\* $NUM_THREADS_PLUS_ONE`}
 
 HISTORICAL_JAVA_OPTS="-XX:NewSize=6g -XX:MaxNewSize=6g -XX:+UseConcMarkSweepGC -XX:MaxDirectMemorySize=${JVM_MAX_DIRECT_MEM_SIZE}"
-HISTORICAL_JAVA_PROPS="-Ddruid.server.tier=${HISTORICAL_TIER} -Ddruid.storageDirectory=${DEEPSTORAGE_DIRECTORY} -Ddruid.server.http.numThreads=${HTTP_NUM_THREADS} -Ddruid.processing.buffer.sizeBytes=${PROCESSING_BUFFER_SIZE_BYTES} -Ddruid.processing.numThreads=${PROCESSING_NUM_THREADS} -Ddruid.server.maxSize=${SERVER_MAX_SIZE} -Ddruid.segmentCache.locations=[{\"path\":\"/druid/local/historical/indexCache\",\"maxSize\":${SERVER_MAX_SIZE}}] -Ddruid.server.priority=${SERVER_PRIORITY} -Ddruid.historical.cache.useCache=${USE_CACHE} -Ddruid.historical.cache.populateCache=${USE_CACHE}"
+HISTORICAL_JAVA_PROPS="-Ddruid.server.tier=${HISTORICAL_TIER} -Ddruid.storageDirectory=${DEEPSTORAGE_DIRECTORY} -Ddruid.server.http.numThreads=${HTTP_NUM_THREADS} -Ddruid.processing.buffer.sizeBytes=${PROCESSING_BUFFER_SIZE_BYTES} -Ddruid.processing.numThreads=${PROCESSING_NUM_THREADS} -Ddruid.server.maxSize=${SERVER_MAX_SIZE} -Ddruid.segmentCache.locations=[{\"path\":\"/druid/local/historical/indexCache\",\"maxSize\":${SERVER_MAX_SIZE}}] -Ddruid.server.priority=${SERVER_PRIORITY} -Ddruid.historical.cache.useCache=${USE_CACHE} -Ddruid.historical.cache.populateCache=${USE_CACHE} -Ddruid.cache.type=${CACHE_TYPE} -Ddruid.cache.memcachedPrefix=${CACHE_PREFIX} -Ddruid.cache.hosts=${MEMCACHED_HOSTS} -Ddruid.cache.expiration=${CACHE_EXPIRATION}"
 
 
 JAVA_COMMAND="java $COMMON_JAVA_PROPS $HISTORICAL_JAVA_OPTS -cp $CP $COMMON_JAVA_OPTS $HISTORICAL_JAVA_PROPS io.druid.cli.Main server historical"
